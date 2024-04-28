0b8b4a2fe9685d01b64fc83c99f03c7ac62f4028
vkhydras <victorkimaru8@gmail.com>
2024-04-28 20:08:21 +0300

    media

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,6 @@ urlpatterns = [
     path('about/', views.about),
     path('posts/', include('posts.url'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
