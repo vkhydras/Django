3452eda19384c8712d4919655528f2ae6b5e9004
vkhydras <victorkimaru8@gmail.com>
2024-04-28 17:53:31 +0300

    posts

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def posts_list(req):
-    return render(req, 'posts/posts_list.html')
+    posts = Post.objects.all().order_by('-date')
+    return render(req, 'posts/posts_list.html',{
+        'posts':posts
+    })
